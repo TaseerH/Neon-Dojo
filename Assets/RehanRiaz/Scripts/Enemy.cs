a8c2b9ca9737f5b6fa3bf052e101174aaee64ff1
RehanRiaz20 <rehanriaz991@gmail.com>
2023-08-15 16:16:23 +0500

    enemy ai, animation
    enemy ai and animations.

---
+++ Assets/RehanRiaz/Scripts/Enemy.cs
@@ -0,0 +1,116 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    public NavMeshAgent agent;
+
+    public Transform player;
+    public GameObject gun;
+
+    //Stats
+    public int health;
+
+    //Check for Ground/Obstacles
+    public LayerMask whatIsGround, whatIsTower;
+
+    //Patroling
+    public Vector3 walkPoint;
+    public bool walkPointSet;
+    public float walkPointRange;
+
+    //Attack Player
+    public float timeBetweenAttacks;
+    bool alreadyAttacked;
+
+    //States
+    public bool isDead;
+    public float attackRange;
+    public bool playerInSightRange, playerInAttackRange;
+
+    //Special
+    public Material green, red, yellow;
+    public GameObject projectile;
+
+    private Animator enemyAnimator;
+    private void Awake()
+    {
+        enemyAnimator = GetComponent<Animator>();
+        player = GameObject.Find("Tower").transform;
+        agent = GetComponent<NavMeshAgent>();
+        attackRange = Random.Range(30f, 60f);
+    }
+    private void Update()
+    {
+        if (!isDead)
+        {
+            //Check if Player in sightrange
+            // playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsTower);
+            playerInSightRange = true;
+            //Check if Player in attackrange
+            playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsTower);
+
+            if (playerInSightRange && !playerInAttackRange) ChasePlayer();
+            if (playerInAttackRange && playerInSightRange) AttackPlayer();
+        }
+    }
+
+    //private void OnDrawGizmos()
+    //{
+    //    Gizmos.DrawSphere(transform.position, attackRange);
+    //}
+
+
+    private void SearchWalkPoint()
+    {
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        if (Physics.Raycast(walkPoint, -transform.up, 2, whatIsGround))
+            walkPointSet = true;
+    }
+    private void ChasePlayer()
+    {
+        if (isDead) return;
+
+        agent.SetDestination(player.position);
+       // enemyAnimator.SetTrigger("EnemyRifleRun");
+        // GetComponent<MeshRenderer>().material = yellow;
+    }
+
+    private void AttackPlayer()
+    {
+        if (isDead) return;
+
+        //Make sure enemy doesn't move
+        agent.SetDestination(transform.position);
+        Debug.Log("player in attack range.");
+
+        transform.LookAt(player);
+        enemyAnimator.SetTrigger("ShootNow");
+        /*  if (!alreadyAttacked)
+          {
+
+              //Attack
+              Rigidbody rb = Instantiate(projectile, transform.position, Quaternion.identity).GetComponent<Rigidbody>();
+
+              rb.AddForce(transform.forward * 32f, ForceMode.Impulse);
+              rb.AddForce(transform.up * 8, ForceMode.Impulse);
+
+              alreadyAttacked = true;
+              Invoke("ResetAttack", timeBetweenAttacks);
+         }*/
+
+        //GetComponent<MeshRenderer>().material = red;
+    }
+}
+
+
+
+
---
+++ Assets/RehanRiaz/Scripts/EnemySpawner.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public int spawnAfterSec;
+    public GameObject[] enemiesToSpawn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnNow", 3, spawnAfterSec);
+        
+    }
+
+    // Update is called once per frame
+    Vector3 GetRandomPosition()
+    {
+        int randomCorner = Random.RandomRange(1, 5);
+        float _x, _y, _z;
+        if (randomCorner==1)
+        {
+            _z = -70f;
+            _y = -1.607807f;
+            _x = Random.RandomRange(-70f, 67f);
+        }
+        else if (randomCorner == 2)
+        {
+            _z = 66f;
+            _y = -1.607807f;
+            _x = Random.RandomRange(-70f, 67f);
+        }
+        else if (randomCorner == 3)
+        {
+            _x = -70f;
+            _y = -1.607807f;
+            _z = Random.RandomRange(-70f, 67f);
+        }
+        else
+        {
+            _x = 66f;
+            _y = -1.607807f;
+            _z = Random.RandomRange(-70f, 67f);
+        }
+
+ 
+        Vector3 newPosition = new Vector3(_x,_y,_z);
+        return newPosition;
+    }
+
+
+    void SpawnNow()
+    {
+        Instantiate(enemiesToSpawn[Random.RandomRange(0, 2)], GetRandomPosition(), Quaternion.identity);
+    }
+}
